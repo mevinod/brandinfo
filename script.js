3028f8ba7e4cb76efe180cbae801bf861d02e722
vinod-1 <vinod@plank.life>
2020-08-17 00:39:52 +0530

    testing designs

--- script.js
+++ script.js
@@ -1,11 +1,71 @@
-const searchBar = document.querySelector('.search-bar')
-const searchInput = document.querySelector('.search-input')
-const searchBtn = document.querySelector('.search-btn')
-searchBtn.addEventListener('click', ()=>{
-  if(!searchBar.classList.contains('active')) {
-    searchBar.classList.add('active')
-    searchInput.focus()
-  } else {
-    searchBar.classList.remove('active')
-  }
-})
+/*!
+  Author     : Douglas "Amarelo" Lopes
+  Author URI : http://douglasamarelo.com/
+  Version    : 1.0.0
+  Repository : https://github.com/DouglasAmarelo/filtro-com-destaque
+*/
+
+// List of cities
+const cities = [
+	"Xiaomi",
+	"Peter England",
+	"Samsung",
+	"Motorola",
+	"Nokon",
+	"Mahindra",
+	"TATA"];
+
+// Selected DOM elements
+const $search = document.querySelector(".search");
+const $list = document.querySelector(".list");
+const $listFilteredItems = document.querySelector(".filtered");
+const $listToralItems = document.querySelector(".total");
+
+// Update DOM with the cities
+const inertCitiesIntoDOM = (cities) => {
+	const allCities = cities.map((city) => `<li>${city}</li>`).join("");
+	$list.innerHTML = allCities;
+};
+
+// Show the total size of the array of cities
+const updateTotalItems = (list) => ($listToralItems.innerHTML = list.length);
+
+const updateFilteredItems = (list) =>
+	($listFilteredItems.innerHTML = list.length);
+
+// Get a text, clean and return a regex
+const transformTextToRegex = (text) => {
+	const result = text.trim().replace(/\(|\)/gim, "");
+
+	return new RegExp(`(${result})`, "gmi");
+};
+
+// Return an Array filtered
+const filterByRegex = (list, regex) => list.filter((item) => item.match(regex));
+
+// Select the text that matchs with the searchterm
+const highlightTextByRegex = (searchTerm, regex) => {
+	$list.querySelectorAll("li").forEach((item) => {
+		if (!searchTerm) return (item.innerHTML = item.textContent);
+
+		item.innerHTML = item.textContent.replace(
+			regex,
+			'<span class="selected">$1</span>'
+		);
+	});
+};
+
+// Listen to user text input
+$search.addEventListener("input", (e) => {
+	const searchTerm = e.target.value;
+	const searchRegex = transformTextToRegex(searchTerm);
+	const filteredCities = filterByRegex(cities, searchRegex);
+
+	inertCitiesIntoDOM(searchTerm ? filteredCities : cities);
+	updateFilteredItems(searchTerm ? filteredCities : []);
+	highlightTextByRegex(searchTerm, searchRegex);
+});
+
+// Start app
+inertCitiesIntoDOM(cities);
+updateTotalItems(cities);
